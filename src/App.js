685f5084e94901d0aca19c5f84646f18e7370c35
ohziiilov3 <klbaskerville0520@gmail.com>
2022-10-13 21:31:28 -0700

    created react app added todo component and usestate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React , {useState} from 'react';
+import {AiOutlinePlus} from 'react-icons/ai'
+import Todo from './components/Todo';
+
+
+
+const style = {
+bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#e76f51] to-[#f4a261]`
+}
+
 
 function App() {
+
+  const [todos, setTodos] = useState(['Learn React','Daily Coding Challenges'])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={style.bg}>
+     <div className={style.container}>
+      <h3 className={style.heading}>Todo App</h3>
+      <form className={style.from}>
+        <input className={style.input} type="text" placeholder="Add Todo"/>
+        <button className={style.button}><AiOutlinePlus size={30}/></button>
+      </form>
+      <ul>
+        {todos.map((todo, idx )=> (
+          <Todo key={idx} todo={todo} />
+
+        ))}
+      </ul>
+     </div>
     </div>
   );
 }
