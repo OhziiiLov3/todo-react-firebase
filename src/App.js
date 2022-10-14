58eba2a6dd5b3fa0e1541d5a1758aed19a8c6bb1
ohziiilov3 <klbaskerville0520@gmail.com>
2022-10-13 21:56:39 -0700

    tailwind styling adding to todo component and form

--- src/App.js
+++ src/App.js
@@ -5,7 +5,13 @@ import Todo from './components/Todo';
 
 
 const style = {
-bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#e76f51] to-[#f4a261]`
+bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#e76f51] to-[#f4a261]`,
+container: `bg-slate-100 max-w-[600px] w-full m-auto rounded-md shadow-xl p-4 py-2`,
+heading: `text-3xl font-bold text-center text-gray-800 p-2`,
+form: `flex justify-between`,
+input: `border p-2 w-full text-xl`,
+button: `border p-4 ml-2 bg-[#2a9d8f] text-slate-100`, 
+count: `text-center p-2`
 }
 
 
@@ -17,9 +23,11 @@ function App() {
     <div className={style.bg}>
      <div className={style.container}>
       <h3 className={style.heading}>Todo App</h3>
-      <form className={style.from}>
+      <form className={style.form}>
         <input className={style.input} type="text" placeholder="Add Todo"/>
-        <button className={style.button}><AiOutlinePlus size={30}/></button>
+        <button className={style.button}>
+          <AiOutlinePlus size={30}/>
+          </button>
       </form>
       <ul>
         {todos.map((todo, idx )=> (
@@ -27,6 +35,7 @@ function App() {
 
         ))}
       </ul>
+      <p className={style.count}>You have 2 todos</p>
      </div>
     </div>
   );
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -3,6 +3,12 @@ import {FaRegTrashAlt} from 'react-icons/fa'
 
 
 const style = {
+    li: `flex justify-between bg-slate-200 p-4 my-2 capitalize`,
+    liComplete: `flex justify-between bg-slate-400 p-4 my-2 capitalize`,
+    row: `flex`,
+    text: `ml-2 cursor-pointer`,
+    textComplete: ` ml-2 cursor-pointer line-through`,
+    button: `cursor-pointer flex items-center`
 
 }
 
